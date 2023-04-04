afec91af43ac9cfd3e9270c5166bd4162f5cd81d
benjamin.williams <benjamin.williams1020@gmail.com>
2023-04-03 23:33:31 -0400

    Completed Lesson 8: PHP Functions

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,11 @@
-This is our SUPER amazing custom theme.
+<?php
+    function greet($name, $color) {
+        echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
+    }
+
+    greet('John', 'blue');
+    greet('Jane', 'green');
+?>
+
+<h1><?php bloginfo('name'); ?></h1> <!-- name comes from the WordPress dashboard under Settings > Site Title -->
+<p><?php bloginfo('description'); ?></p> <!-- description comes from the WordPress dashboard under Settings > Tagline -->
