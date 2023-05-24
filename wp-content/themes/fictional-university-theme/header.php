793e0a1e43058e9d4aaffa597047fd284217dc61
benjamin.williams <benjamin.williams1020@gmail.com>
2023-05-24 00:29:16 -0400

    Completed lesson 21. Code works as expected.

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -22,7 +22,7 @@
             //This section is for the dynamic menus generated in the WordPress admin area
             ?>            
             <ul>
-              <li><a href="<?php echo site_url('about-us'); ?>">About Us</a></li>
+              <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 14) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
