49541d23f22e03fe4d0427565ae9f9b82d0e9f78
benjamin.williams <benjamin.williams1020@gmail.com>
2023-09-14 00:18:53 -0400

    Completed Lesson 36. All code works as expected.

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,10 +1,12 @@
-<?php 
-function university_post_types() {
-
+<?php
+function university_post_types()
+{
     //Event Post Type
     register_post_type('event', array(
-        'supports' => array('title', 'editor', 'excerpt'), //here editor is referring to the modern WP editor
-        'rewrite' => array('slug' => 'events'), //this rewrites the slug for his post type
+        'supports' => array('title', 'editor', 'excerpt'),
+        //here editor is referring to the modern WP editor
+        'rewrite' => array('slug' => 'events'),
+        //this rewrites the slug for his post type
         'has_archive' => true,
         'public' => true,
         'show_in_rest' => true,
@@ -17,9 +19,26 @@ function university_post_types() {
         ),
         'menu_icon' => 'dashicons-calendar'
     ));
-    
-    
 
+    //Program Post Type
+    register_post_type('program', array(
+        'supports' => array('title', 'editor'),
+        //here editor is referring to the modern WP editor
+        'rewrite' => array('slug' => 'programs'),
+        //this rewrites the slug for his post type
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
+    ));
+}
 add_action('init', 'university_post_types') //this function initializes a new function: university_post_types()
 
-?>
+    ?>
